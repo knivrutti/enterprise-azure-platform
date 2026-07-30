bd2878887e8353b72fe028fe0ce88bcb08d71894
Knivrutti <knivrutti@gmail.com>
2026-07-30 10:24:21 -0400

    Configure AzureRM provider and initial local settings

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  location = "East US"
+
+  resource_group_name = "kml_rg_main-d0c045516abe408f"
+
+  common_tags = {
+    Environment = "Dev"
+    Project     = "EnterprisePlatform"
+    ManagedBy   = "Terraform"
+  }
+}
